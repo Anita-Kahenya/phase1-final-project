fcfad819ea357ca755aa7ac833872a30da8eaaf3
joanne muthoni <joanne.mumbi@student.moringaschool.com>
2022-12-17 16:55:16 +0300

    display all the  images on the web

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const option= document.getElementById("categories")
 option.addEventListener("mouseover", handlehover)
 function handlehover (event){
-
-}
-function getImageData(url){
-    fetch(url)
-    .then(res=>res.json())
-    .then(data => GetData(data))
-}
-getImageData( "http://localhost:3000/art")
-function GetData( art){
     
 
+}
+function fetchData(url){
+    fetch(url)
+    .then(res=>res.json())
+    .then(arts=> {
+        GetDetails(arts)})
+}
+fetchData( "http://localhost:3000/art")
+
+function GetDetails(arts){
+    for (let art of arts){
+       console.log(displayArt(art))
+    }  
+}
+function displayArt(detail){
+    const showArt= document.querySelector("#artisty")
+    let card=document.createElement("div")
+    let img= document.createElement("img")
+    img.setAttribute("src", detail.image)
+    card.appendChild(img)
+    showArt.appendChild(card)
 }
